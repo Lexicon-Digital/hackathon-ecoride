9fc5a4d0e1403fe68b1b7e1769b85a71e0ff6452
braydend <braydendiluca@gmail.com>
2021-12-21 12:43:49 +1100

    Add Join Organisation view to navigation flow

--- HitchHiker/components/Navigation/Navigation.tsx
+++ HitchHiker/components/Navigation/Navigation.tsx
@@ -4,6 +4,9 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { SignUp } from '../SignUp/SignUp'
 import { Map } from '../Map'
 import { Login } from '../Login/Login'
+import {JoinOrganisation} from "../../views/JoinOrganisation";
+import {getData} from "../../utils/AsyncStorage";
+import {Text} from "react-native";
 
 const Stack = createNativeStackNavigator();
 
@@ -11,16 +14,42 @@ export enum EScreens {
     SIGN_UP = 'SignUp',
     LOGIN = 'Login',
     MAP = 'Map',
+    JOIN = 'JoinOrg',
 }
 
 export const Navigation = () => {
-    return (
-        <NavigationContainer>
-            <Stack.Navigator initialRouteName={EScreens.SIGN_UP}>
-                <Stack.Screen name={EScreens.SIGN_UP} component={SignUp} options={{title: 'Sign Up'}} />
-                <Stack.Screen name={EScreens.LOGIN} component={Login} options={{title: 'Login'}} />
-                <Stack.Screen name={EScreens.MAP} component={Map} />
-            </Stack.Navigator>
-        </NavigationContainer>
-    );
+    const [initialRoute, setInitialRoute] = React.useState<string>(/*EScreens.SIGN_UP*/"JoinOrg")
+    const [orgToJoin, setOrgToJoin] = React.useState<string>("")
+    const [isLoading, setIsLoading] = React.useState(true)
+
+    const JoinOrganisationWithName: React.VFC<{navigation: Navigation}> = (props) => <JoinOrganisation organisationName={orgToJoin} {...props} />;
+
+    React.useEffect(() => {
+        (async () => {
+            const orgName = await getData<string>("joinOrganisation")
+
+            if (orgName) {
+                setOrgToJoin(orgName)
+                setInitialRoute(EScreens.JOIN)
+            }
+        })()
+        setIsLoading(false)
+    }, [])
+
+    return isLoading
+        ? <Text>Loading...</Text>
+        : (
+            <NavigationContainer>
+                <Stack.Navigator initialRouteName={initialRoute}>
+                    <Stack.Screen name={EScreens.SIGN_UP} component={SignUp} options={{title: 'Sign Up'}} />
+                    <Stack.Screen name={EScreens.LOGIN} component={Login} options={{title: 'Login'}} />
+                    <Stack.Screen name={EScreens.MAP} component={Map} />
+                    <Stack.Screen name={EScreens.JOIN} component={JoinOrganisationWithName}/>
+                </Stack.Navigator>
+            </NavigationContainer>
+        );
+};
+
+export type Navigation = {
+    navigate: (routeName: string) => void;
 };
--- HitchHiker/views/JoinOrganisation/JoinOrganisation.tsx
+++ HitchHiker/views/JoinOrganisation/JoinOrganisation.tsx
@@ -1,9 +1,11 @@
 import React from "react";
-import {Button, SafeAreaView, StyleSheet, Text} from "react-native";
 import { Text as RNEText } from "react-native-elements";
+import {Button, SafeAreaView, StyleSheet, Text, ToastAndroid} from "react-native";
+import {Navigation} from "../../components/Navigation/Navigation";
 
 type JoinOrganisationProps = {
     organisationName: string;
+    navigation: Navigation;
 };
 
 const styles = StyleSheet.create({
@@ -21,15 +23,20 @@ const styles = StyleSheet.create({
     }
 });
 
-export const JoinOrganisation: React.VFC<JoinOrganisationProps> = ({ organisationName }) => {
+export const JoinOrganisation: React.VFC<JoinOrganisationProps> = ({ organisationName, navigation }) => {
+    const handleJoin = async () => {
+        await new Promise(resolve => { setTimeout(resolve, 4000)});
 
-    const handleJoinOrganisation = () => {};
+        ToastAndroid.show(`You have been accepted into ${organisationName}'s organisation`, 1000);
+
+        navigation.navigate("SignUp")
+    };
 
     return (
         <SafeAreaView style={styles.joinOrg}>
             <RNEText h1 style={styles.title}>Hitch Hiker</RNEText>
             <Text style={styles.inviteMessage}>You've been invited to join {organisationName}'s organisation!</Text>
-            <Button title={`Join ${organisationName}`} onPress={handleJoinOrganisation} />
+            <Button title={`Request to join ${organisationName}`} onPress={handleJoin} />
         </SafeAreaView>
     );
 };
