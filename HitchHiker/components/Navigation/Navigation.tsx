b57c69d37f03632fb74762427e2fa26a1b4c6944
Jake Bayer <jake.bayer@ioof.com.au>
2021-12-21 17:28:54 +1100

    pretty header

--- HitchHiker/components/Navigation/Navigation.tsx
+++ HitchHiker/components/Navigation/Navigation.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Text } from "react-native";
+import { Image, Text } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { SignUp } from '../SignUp/SignUp'
@@ -8,6 +8,7 @@ import { Login } from '../Login/Login'
 import { CarpoolList } from '../CarpoolList/CarpoolList'
 import { JoinOrganisation } from "../../views/JoinOrganisation";
 import { getData } from "../../utils/AsyncStorage";
+const logo = require("../../resources/images/eco-logo.png");
 
 const Stack = createNativeStackNavigator();
 
@@ -38,18 +39,47 @@ export const Navigation = () => {
             }
         })()
         setIsLoading(false)
-    }, [])
+    }, []);
+
+    const headerStyles = {
+        backgroundColor: '#000',
+    };
+    const headerTitleStyles = {
+        color: '#FFF'
+    }
+
+    const Logo = () => <Image source={logo} style={{width:250, height:50}} />
 
     return isLoading
-        ? <Text>Loading...</Text>
+        ? <><Text>Loading...</Text></>
         : (
             <NavigationContainer>
                 <Stack.Navigator initialRouteName={initialRoute}>
-                    <Stack.Screen name={EScreens.SIGN_UP} component={SignUp} options={{title: 'Sign Up'}} />
-                    <Stack.Screen name={EScreens.LOGIN} component={Login} options={{title: 'Login'}} />
-                    <Stack.Screen name={EScreens.CARPOOL_LIST} component={CarpoolList} options={{title: 'Carpool List'}}  />
-                    <Stack.Screen name={EScreens.MAP} component={MapWithMarkers} />
-                    <Stack.Screen name={EScreens.JOIN} component={JoinOrganisationWithName} options={{title: 'Join Organisation'}}/>
+                    <Stack.Screen
+                        name={EScreens.SIGN_UP}
+                        component={SignUp}
+                        options={{headerTitle: () => <Logo />, headerTintColor: '#fff', headerStyle: headerStyles, headerTitleStyle: headerTitleStyles }}
+                    />
+                    <Stack.Screen
+                        name={EScreens.LOGIN}
+                        component={Login}
+                        options={{headerTitle: () => <Logo />, headerTintColor: '#fff', headerStyle: headerStyles, headerTitleStyle: headerTitleStyles}}
+                    />
+                    <Stack.Screen
+                        name={EScreens.CARPOOL_LIST}
+                        component={CarpoolList}
+                        options={{headerTitle: () => <Logo />, headerTintColor: '#fff', headerStyle: headerStyles, headerTitleStyle: headerTitleStyles}}
+                    />
+                    <Stack.Screen
+                        name={EScreens.MAP}
+                        component={MapWithMarkers}
+                        options={{headerTitle: () => <Logo />, headerTintColor: '#fff', headerStyle: headerStyles, headerTitleStyle: headerTitleStyles}}
+                    />
+                    <Stack.Screen
+                        name={EScreens.JOIN}
+                        component={JoinOrganisationWithName}
+                        options={{headerTitle: () => <Logo />, headerTintColor: '#fff', headerStyle: headerStyles, headerTitleStyle: headerTitleStyles}}
+                    />
                 </Stack.Navigator>
             </NavigationContainer>
         );
