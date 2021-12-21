57525138c6af28c4ebc11b162d98e01369c025aa
braydend <braydendiluca@gmail.com>
2021-12-21 13:19:55 +1100

    Add progress bar and confirmation toast

--- HitchHiker/views/JoinOrganisation/JoinOrganisation.tsx
+++ HitchHiker/views/JoinOrganisation/JoinOrganisation.tsx
@@ -1,6 +1,6 @@
-import React from "react";
-import { Text as RNEText } from "react-native-elements";
-import {Button, SafeAreaView, StyleSheet, Text, ToastAndroid} from "react-native";
+import React, {useState} from "react";
+import {LinearProgress, Overlay, Text as RNEText} from "react-native-elements";
+import {Button, SafeAreaView, StyleSheet, Text, ToastAndroid, View} from "react-native";
 import {Navigation} from "../../components/Navigation/Navigation";
 
 type JoinOrganisationProps = {
@@ -24,11 +24,15 @@ const styles = StyleSheet.create({
 });
 
 export const JoinOrganisation: React.VFC<JoinOrganisationProps> = ({ organisationName, navigation }) => {
+    const [isLoading, setIsLoading] = useState(false);
     const handleJoin = async () => {
+        setIsLoading(true)
+        // Fake wait to simulate request being confirmed by admin
         await new Promise(resolve => { setTimeout(resolve, 4000)});
 
         ToastAndroid.show(`You have been accepted into ${organisationName}'s organisation`, 1000);
 
+        setIsLoading(false)
         navigation.navigate("SignUp")
     };
 
@@ -36,7 +40,10 @@ export const JoinOrganisation: React.VFC<JoinOrganisationProps> = ({ organisatio
         <SafeAreaView style={styles.joinOrg}>
             <RNEText h1 style={styles.title}>Hitch Hiker</RNEText>
             <Text style={styles.inviteMessage}>You've been invited to join {organisationName}'s organisation!</Text>
-            <Button title={`Request to join ${organisationName}`} onPress={handleJoin} />
+            <View>
+                {isLoading && <LinearProgress />}
+                <Button title={`Request to join ${organisationName}`} onPress={handleJoin} disabled={isLoading} />
+            </View>
         </SafeAreaView>
     );
 };
